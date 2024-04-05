91b9236cae3b9dd26a4e29cb072d4fe628f4b0c1
Adam Boman <adamoboman@gmail.com>
2024-04-05 10:00:21 +0200

    Added character with movement

---
+++ Assets/Characters/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Animator playerAnimation;
+    public Rigidbody playerRigid;
+    public float w_speed, rotation_speed;
+    public Transform playerTransform;
+    public bool walking, idle;
+
+    void FixedUpdate()
+    {
+        //movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        playerRigid.velocity = movementDirection * w_speed * Time.deltaTime;
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
+            playerTransform.rotation = Quaternion.Slerp(playerTransform.rotation, targetRotation, rotation_speed * Time.deltaTime);
+        }
+        
+    }
+
+    void Update()
+    {
+        //animation 
+        bool anyKeyPressed = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+
+        walking = anyKeyPressed;
+        idle = !anyKeyPressed;
+
+        playerAnimation.SetBool("Walking", walking);
+        playerAnimation.SetBool("Idle", idle);
+
+        if(!anyKeyPressed)
+        {
+            playerRigid.velocity = Vector3.zero;
+        }
+    }
+
+
+
+
+
+    
+}
