5c0ffdb9ede06cfb52e96cbc94a1e351ae533032
Adam Boman <adamoboman@gmail.com>
2024-04-16 12:34:33 +0200

    Add WordSearchGridGenerator class to generate and instantiate a grid in WordMiniGame

--- Assets/scripts/WordMiniGameScripts/WordSearchGridGenerator.cs
+++ Assets/scripts/WordMiniGameScripts/WordSearchGridGenerator.cs
@@ -2,12 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Word : MonoBehaviour
+public class WordSearchGridGenerator : MonoBehaviour
 {
+
+    public int rows;
+    public int columns;
+    public GameObject cellPrefab;
+
+    private char[,] grid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateGrid();
+        InstantiateGrid();
+    }
+
+    GenerateGrid()
+    {
+        grid = new char[rows, columns];
+    }
+
+    InstantiateGrid()
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                Vector3 cellPosition = new Vector3(row, column, 0);
+                GameObject cell = Instantiate(cellPrefab, new Vector3(row, column, 0), Quaternion.identity);
+                cell.transform.SetParent(this.transform);
+            }
+        }
     }
 
     // Update is called once per frame
