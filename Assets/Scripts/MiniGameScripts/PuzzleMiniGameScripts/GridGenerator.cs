c0509167ab3ff9836a8127d709892b21a5779ad2
niljenn <jennifernilsson0125@gmail.com>
2024-05-01 12:37:58 +0200

    structure of puzzle mini game finsihed with event handler but no function

--- Assets/Scripts/MiniGameScripts/PuzzleMiniGameScripts/GridGenerator.cs
+++ Assets/Scripts/MiniGameScripts/PuzzleMiniGameScripts/GridGenerator.cs
@@ -6,34 +6,48 @@ public class GridGenerator : MonoBehaviour
 {
     public int gridRows;
     public int gridCols;
-    public GameObject cube;
     public GameObject image;  //Quad object
+    public float cellSize = 4.0f;
+
+    //private List<Transform> pieces;
 
     // Start is called before the first frame update
     void Start()
     {
+        //pieces = new List<Transform>();
         GenerateGrid();
     }
 
     void GenerateGrid(){
 
-        float cellSize = 2.0f;
-        float distanceBetweenCells = 0.2f;
+        float distanceBetweenCells = 0.1f;
 
         for (int row = 0; row < gridRows; row++){
 
             for (int col = 0; col < gridCols; col++){
 
-                Vector3 generatePosition = new Vector3(row * (cellSize + distanceBetweenCells), 0, col * (cellSize + distanceBetweenCells));
-                GameObject gridCell = Instantiate(cube, generatePosition, Quaternion.identity);
+                // Calculate UV coordinates to divide the image among the quad objects
+                Vector2[] uv = new Vector2[4];
+                uv[0] = new Vector2((float)col / gridCols,((float)row / gridRows));
+                uv[1] = new Vector2((float)(col + 1) / gridCols,((float)row / gridRows));
+                uv[2] = new Vector2((float)col / gridCols,((float)(row + 1) / gridRows));
+                uv[3] = new Vector2((float)(col + 1) / gridCols,((float)(row + 1) / gridRows));
+
+                Vector3 generatePosition = new Vector3(col * (cellSize + distanceBetweenCells), 0, row * (cellSize + distanceBetweenCells));
+
                 GameObject imageQuad = Instantiate(image, generatePosition, Quaternion.identity);
-                imageQuad.transform.SetParent(gridCell.transform);
+
+                imageQuad.transform.localScale = new Vector3(cellSize, cellSize, cellSize);
+
+                // Set UV coordinates to the quad object
+                Mesh mesh = imageQuad.transform.GetChild(0).GetComponent<MeshFilter>().mesh;
+                mesh.uv = uv;
+
+                imageQuad.transform.name = $"{(col * (gridRows * gridCols) + row)}";
 
                 if ((row == gridRows - 1) && (col == gridCols - 1)) {
-                    gridCell.gameObject.SetActive(false);
-
+                    imageQuad.gameObject.SetActive(false);
                 }
-
             }
         }
     }
@@ -41,6 +55,29 @@ public class GridGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)){
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Perform the raycast
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+
+                MeshCollider meshCollider = hit.collider as MeshCollider;
+                if (meshCollider != null){
+
+                    HandleClickEvent(hit.transform);
+                }
+
+
+            }
+
+        }
     }
+    
+    void HandleClickEvent (Transform clickedTransform){
+        Debug.Log("Quad clicked: " + clickedTransform.name);
+    }
+
+
 }
