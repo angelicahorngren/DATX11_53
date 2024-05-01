2a88685712e629c10576793a4c4803b96630dab3
niljenn <jennifernilsson0125@gmail.com>
2024-05-01 21:15:58 +0200

    added dialogueBox feature

---
+++ Assets/Scenes/DialogueBoxScenes/DialogueLogic.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class DialogueLogic : MonoBehaviour
+{
+    public TextMeshProUGUI introText;
+    public TextMeshProUGUI continuationText;
+
+    public GameObject panel;
+  
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        introText.text = string.Empty;
+        StartDialogue();
+    }
+
+
+    void StartDialogue() {
+
+        index = 0;
+        StartCoroutine(TypeLine());
+
+    }
+
+    public void ClickContinue(){
+
+        if (introText.text == lines[index]){
+                panel.SetActive(true);
+                NextLine();
+            }
+            else {
+                StopTypingAndPrintLine();
+            }
+    }
+
+    public void SecondClickContinue(){
+        if (continuationText.text == lines[index] && index < lines.Length - 1){
+            NextLine();
+        }
+        else {
+            StopTypingAndPrintLine();
+        }
+    }
+
+    public void ClickBack(){
+        if (continuationText.text == lines[index] && index <= lines.Length - 1){
+            PreviousLine();
+        }
+        else {
+            StopTypingAndPrintLine();
+        }
+    }
+
+    void StopTypingAndPrintLine(){
+        StopAllCoroutines();
+        introText.text = lines[index];
+        continuationText.text = lines[index];
+    }
+
+
+    IEnumerator TypeLine(){
+
+        foreach(char c in lines[index].ToCharArray()){
+
+            if (index == 0){
+                introText.text += c;
+
+            }else {
+                continuationText.text += c;
+            }
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine(){
+
+        index++;
+        continuationText.text = string.Empty;
+        StartCoroutine(TypeLine());
+    }
+    
+
+    void PreviousLine(){
+
+        index--;
+        continuationText.text = lines[index];
+    }
+}
+
+
