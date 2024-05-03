f949bb3a3d447dd47f9eafa1f6fd552268725c53
Marsaielda <ida.altenstedt@gmail.com>
2024-05-03 17:22:30 +0200

    new skript for enum of what player is holding

---
+++ Assets/Scripts/PlayerScripts/PlayerItemHandlerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerItemHandlerScript : MonoBehaviour
+{
+
+    private enum EnumItems {Empty, MagnifyingGlass, NotePad}
+    [SerializeField] EnumItems ItemInHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
