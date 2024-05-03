22ad265691d6ce7367a380bd0b25d86618fa3e40
Marsaielda <ida.altenstedt@gmail.com>
2024-05-03 17:38:05 +0200

    added Blacklight to the items enum

--- Assets/Scripts/PlayerScripts/PlayerItemHandlerScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerItemHandlerScript.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerItemHandlerScript : MonoBehaviour
 {
 
-    private enum EnumItems {Empty, MagnifyingGlass, NotePad}
+    private enum EnumItems {Empty, MagnifyingGlass, NotePad, Blacklight}
     [SerializeField] EnumItems ItemInHand;
     // Start is called before the first frame update
     void Start()
