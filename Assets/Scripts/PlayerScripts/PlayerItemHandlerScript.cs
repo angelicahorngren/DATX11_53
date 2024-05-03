9f7461fd449f5d9b6116b849d55c5e1ba99a065a
Marsaielda <ida.altenstedt@gmail.com>
2024-05-03 17:42:50 +0200

    get and set methods for item in hand

--- Assets/Scripts/PlayerScripts/PlayerItemHandlerScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerItemHandlerScript.cs
@@ -1,22 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerItemHandlerScript : MonoBehaviour
 {
 
-    private enum EnumItems {Empty, MagnifyingGlass, NotePad, Blacklight}
-    [SerializeField] EnumItems ItemInHand;
+    enum EnumItems {Empty, MagnifyingGlass, NotePad, Blacklight}
+    [SerializeField] private EnumItems ItemInHand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ItemInHand = EnumItems.Empty;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DropItem(){
+        ItemInHand = EnumItems.Empty;
+    }
+
+    void PickUpMagnifyingGlass(){
+        ItemInHand = EnumItems.MagnifyingGlass;
+    }
+    void PickUpNotePad(){
+        ItemInHand = EnumItems.NotePad;
+    }
+    void PickUpBlacklight(){
+        ItemInHand = EnumItems.Blacklight;
+    }
+
+    EnumItems GetItemInHand(){
+        return ItemInHand;
     }
 }
